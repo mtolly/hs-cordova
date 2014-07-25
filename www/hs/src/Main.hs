e9c6e655894a3f96615c4c0bafe874933e9ff599
Michael Tolly <miketolly@gmail.com>
2014-07-25 16:27:14 -0500

    implemented index.js, doesn't work yet

--- www/hs/src/Main.hs
+++ www/hs/src/Main.hs
@@ -1,4 +1,48 @@
-module Main where
+{-# LANGUAGE OverloadedStrings #-}
+module Main (main) where
+
+import GHCJS.Types
+import GHCJS.Foreign
 
 main :: IO ()
-main = return ()
+main = bindEvents
+
+foreign import javascript safe
+  "document.addEventListener($1, $2, false);"
+  addEventListener :: JSString -> JSFun (IO ()) -> IO ()
+
+bindEvents :: IO ()
+bindEvents = do
+  fn <- asyncCallback AlwaysRetain onDeviceReady
+  addEventListener "deviceready" fn
+
+onDeviceReady :: IO ()
+onDeviceReady = do
+  receivedEvent "deviceready"
+
+data Element_
+type Element = JSRef Element_
+
+foreign import javascript unsafe
+  "document.getElementById($1)"
+  getElementById :: JSString -> IO Element
+
+foreign import javascript unsafe
+  "$1.querySelector($2)"
+  querySelector :: Element -> JSString -> IO Element
+
+foreign import javascript unsafe
+  "$1.setAttribute($2, $3);"
+  setAttribute :: Element -> JSString -> JSString -> IO ()
+
+receivedEvent :: JSString -> IO ()
+receivedEvent evt = do
+
+  parentElement <- getElementById evt
+  listeningElement <- querySelector parentElement ".listening"
+  receivedElement <- querySelector parentElement ".received"
+
+  setAttribute listeningElement "style" "display: none;"
+  setAttribute receivedElement "style" "display: block;"
+
+  putStrLn $ "Received Event: " ++ fromJSString evt
