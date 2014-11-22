a99071dc235f1fbaec2c989cae59f09054d70240
Michael Tolly <miketolly@gmail.com>
2014-11-22 17:54:51 -0600

    cleaned up Device.erb boilerplate

--- hs/src/lib/System/Cordova/Device.hs
+++ hs/src/lib/System/Cordova/Device.hs
@@ -6,23 +6,44 @@ module System.Cordova.Device
 import GHCJS.Types (JSString)
 import GHCJS.Foreign (fromJSString)
 
-foreign import javascript unsafe "device.cordova" js_cordova :: JSString
+
+
+foreign import javascript unsafe
+  "device.cordova"
+  js_cordova :: JSString
 cordova :: String
 cordova = fromJSString js_cordova
 
-foreign import javascript unsafe "device.model" js_model :: JSString
+
+
+foreign import javascript unsafe
+  "device.model"
+  js_model :: JSString
 model :: String
 model = fromJSString js_model
 
-foreign import javascript unsafe "device.platform" js_platform :: JSString
+
+
+foreign import javascript unsafe
+  "device.platform"
+  js_platform :: JSString
 platform :: String
 platform = fromJSString js_platform
 
-foreign import javascript unsafe "device.uuid" js_uuid :: JSString
+
+
+foreign import javascript unsafe
+  "device.uuid"
+  js_uuid :: JSString
 uuid :: String
 uuid = fromJSString js_uuid
 
-foreign import javascript unsafe "device.version" js_version :: JSString
+
+
+foreign import javascript unsafe
+  "device.version"
+  js_version :: JSString
 version :: String
 version = fromJSString js_version
+
 
