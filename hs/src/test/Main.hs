d8ba3ac2b81f6290fae591965a45772ea8699be5
Michael Tolly <miketolly@gmail.com>
2014-12-24 15:06:50 -0600

    device orientation+motion on test app

--- hs/src/test/Main.hs
+++ hs/src/test/Main.hs
@@ -1,15 +1,18 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE LambdaCase #-}
-module Main where
+module Main (main) where
 
 import System.Cordova.Base
 import qualified System.Cordova.StatusBar as Bar
 import qualified System.Cordova.Device as Dev
 import qualified System.Cordova.Geolocation as Geo
+import qualified System.Cordova.DeviceOrientation as DO
+import qualified System.Cordova.DeviceMotion as DM
 import Data.Default
 import GHCJS.Types
 import GHCJS.Foreign
-import Data.IORef
+import Control.Concurrent.MVar
+import Data.Functor (void)
 
 main :: IO ()
 main = do
@@ -26,33 +29,56 @@ main = do
         appendChild elt body
         return elt
 
-  _ <- new "h1" $ "Device"
+  let newToggle :: IO (IO ()) -> IO ()
+      newToggle starter = do
+        stopper <- newMVar Nothing
+        btn <- new "button" "Start listening"
+        onclick btn $ takeMVar stopper >>= \case
+          Nothing -> do
+            newStopper <- starter
+            setHTML "Stop listening" btn
+            putMVar stopper $ Just newStopper
+          Just oldStopper -> do
+            oldStopper
+            setHTML "Start listening" btn
+            putMVar stopper Nothing
+
+  _ <- new "h1" "Device"
   _ <- new "div" $ "cordova: "  ++ show Dev.cordova
   _ <- new "div" $ "model: "    ++ show Dev.model
   _ <- new "div" $ "platform: " ++ show Dev.platform
   _ <- new "div" $ "uuid: "     ++ show Dev.uuid
   _ <- new "div" $ "version: "  ++ show Dev.version
 
-  _ <- new "h1" $ "Geolocation"
-  do
-    posn <- new "div" $ "Position here"
+  _ <- new "h1" "Geolocation"
+  void $ do
+    result <- new "div" $ "Position here"
     btn <- new "button" "Update position"
     onclick btn $ do
       res <- Geo.getCurrentPosition def
-      setHTML (toJSString $ show res) posn
-    toggle <- new "button" "Start listening"
-    stopper <- newIORef Nothing
-    onclick toggle $ readIORef stopper >>= \case
-      Nothing -> do
-        newStopper <- Geo.watchPosition def $ \res ->
-          setHTML (toJSString $ show res) posn
-        writeIORef stopper $ Just newStopper
-        setHTML "Stop listening" toggle
-      Just oldStopper -> do
-        oldStopper
-        setHTML "Start listening" toggle
+      setHTML (toJSString $ show res) result
+    newToggle $ Geo.watchPosition def $ \res ->
+      setHTML (toJSString $ show res) result
 
-  return ()
+  _ <- new "h1" "Device Orientation"
+  void $ do
+    result <- new "div" $ "Direction here"
+    btn <- new "button" "Update direction"
+    onclick btn $ do
+      res <- DO.getCurrentHeading
+      setHTML (toJSString $ show res) result
+    newToggle $ DO.watchHeading def $ \res ->
+      setHTML (toJSString $ show res) result
+
+  _ <- new "h1" "Device Motion"
+  void $ do
+    result <- new "div" $ "Motion here"
+    btn <- new "button" "Update motion"
+    onclick btn $ do
+      res <- DM.getCurrentAcceleration
+      setHTML (toJSString $ show res) result
+    newToggle $ DM.watchAcceleration def $ \res ->
+      setHTML (toJSString $ show res) result
 
 data Element_
 type Element = JSRef Element_
@@ -72,10 +98,6 @@ foreign import javascript unsafe
 foreign import javascript unsafe
   "$2.innerHTML = $1;"
   setHTML :: JSString -> Element -> IO ()
-
-foreign import javascript unsafe
-  "$3.setAttribute($1, $2);"
-  setAttribute :: JSString -> JSString -> Element -> IO ()
 
 foreign import javascript unsafe
   "$1.onclick = $2;"
