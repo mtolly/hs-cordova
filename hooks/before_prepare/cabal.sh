f8695bedbd0c803181e0b91cb13f1ae88221af9c
Michael Tolly <miketolly@gmail.com>
2014-12-24 00:45:45 -0600

    easy way to swap out www/ directory
    in preparation for a hs-cordova-test app

--- hooks/before_prepare/cabal.sh
+++ hooks/before_prepare/cabal.sh
@@ -2,7 +2,11 @@
 set -u
 set -e
 
+# Edit these to choose which Haskell program and html folder to use
 app=hs-cordova-create
+www=www-create
+
+cp -R $www/* www/
 
 cd hs
 make # feel free to delete this if you haven't edited the Ruby files
