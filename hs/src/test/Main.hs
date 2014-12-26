79d3cb10ea62ee66b922755938827cff5492bef3
Michael Tolly <miketolly@gmail.com>
2014-12-25 22:31:07 -0600

    status bar tests; much cleanup

--- hs/src/test/Main.hs
+++ hs/src/test/Main.hs
@@ -10,9 +10,9 @@ import qualified System.Cordova.DeviceOrientation as DO
 import qualified System.Cordova.DeviceMotion as DM
 import qualified System.Cordova.Vibration as Vib
 import qualified System.Cordova.NetworkInformation as Net
-import Data.Default
+import Data.Default (def)
 import GHCJS.Foreign
-import Control.Concurrent.MVar
+import Control.Concurrent.MVar (newMVar, takeMVar, putMVar)
 import Control.Applicative ((<$>))
 import Data.Function (on)
 import Data.Char (isDigit)
@@ -20,7 +20,7 @@ import Data.Maybe (mapMaybe)
 import Data.List (groupBy)
 import Text.Read (readMaybe)
 import Data.Time.Clock (getCurrentTime)
-import Control.Monad.IO.Class
+import Control.Monad.IO.Class (MonadIO, liftIO)
 
 import HTMLT
 
@@ -34,10 +34,10 @@ main = do
 
   setAttribute "style" "font-family: sans-serif;" body
 
-  let newToggle :: (MonadIO m) => IO (IO ()) -> HTMLT m ()
-      newToggle starter = do
+  let toggle :: (MonadIO m) => IO (IO ()) -> HTMLT m ()
+      toggle starter = do
         stopper <- liftIO $ newMVar Nothing
-        "button" </ do
+        button $ do
           btn <- getElement
           text "Start listening"
           onclick $ takeMVar stopper >>= \case
@@ -49,6 +49,15 @@ main = do
               oldStopper
               setHTML "Start listening" btn
               putMVar stopper Nothing
+
+      button :: (MonadIO m) => HTMLT m a -> HTMLT m a
+      button act = "button" </ ("type" $= "button") >> act
+
+      textBox :: (MonadIO m) => HTMLT m Element
+      textBox = "input" <-/ "type" $= "text"
+
+      action :: (MonadIO m) => String -> IO () -> HTMLT m ()
+      action s act = button $ text s >> onclick act
 
   runHTMLT body $ do
 
@@ -66,66 +75,77 @@ main = do
     "h1" </ text "Geolocation"
     do
       result <- "p" <-/ text "Result here"
-      "button" </ do
-        text "Update"
-        onclick $ do
-          res <- Geo.getCurrentPosition def
-          setHTML (toJSString $ show res) result
-      newToggle $ Geo.watchPosition def $ \res ->
+      action "Update" $ do
+        res <- Geo.getCurrentPosition def
+        setHTML (toJSString $ show res) result
+      toggle $ Geo.watchPosition def $ \res ->
         setHTML (toJSString $ show res) result
 
     "h1" </ text "Device Orientation"
     do
       result <- "p" <-/ text "Result here"
-      "button" </ do
-        text "Update"
-        onclick $ do
-          res <- DO.getCurrentHeading
-          setHTML (toJSString $ show res) result
-      newToggle $ DO.watchHeading def $ \res ->
+      action "Update" $ do
+        res <- DO.getCurrentHeading
+        setHTML (toJSString $ show res) result
+      toggle $ DO.watchHeading def $ \res ->
         setHTML (toJSString $ show res) result
 
     "h1" </ text "Device Motion"
     do
       result <- "p" <-/ text "Result here"
-      "button" </ do
-        text "Update"
-        onclick $ do
-          res <- DM.getCurrentAcceleration
-          setHTML (toJSString $ show res) result
-      newToggle $ DM.watchAcceleration def $ \res ->
+      action "Update" $ do
+        res <- DM.getCurrentAcceleration
+        setHTML (toJSString $ show res) result
+      toggle $ DM.watchAcceleration def $ \res ->
         setHTML (toJSString $ show res) result
 
     "h1" </ text "Vibration"
     "form" </ do
-      txt <- "input" <-/ do
-        "type" $= "text"
-      "button" </ do
-        text "Vibrate pattern"
-        "type" $= "button"
-        onclick $ do
-          str <- fromJSString <$> getValue txt
-          let ints = mapMaybe readMaybe $ groupBy ((==) `on` isDigit) str
-          Vib.vibrate ints
-    "button" </ do
-      text "Vibrate cancel"
-      onclick Vib.vibrateCancel
+      txt <- textBox
+      action "Vibrate pattern" $ do
+        str <- fromJSString <$> getValue txt
+        let ints = mapMaybe readMaybe $ groupBy ((==) `on` isDigit) str
+        Vib.vibrate ints
+    action "Vibrate cancel" Vib.vibrateCancel
 
     "h1" </ text "Network Information"
     do
       result <- "p" <-/ text "Result here"
-      "button" </ do
-        text "Update"
-        onclick $ do
-          res <- Net.connectionType
-          setHTML (toJSString $ show res) result
+      action "Update" $ do
+        res <- Net.connectionType
+        setHTML (toJSString $ show res) result
     do
       result <- "p" <-/ text "Offline at:"
-      newToggle $ Net.offlineEvent $ do
+      toggle $ Net.offlineEvent $ do
         time <- getCurrentTime
         setHTML (toJSString $ "Offline at: " ++ show time) result
     do
       result <- "p" <-/ text "Online at:"
-      newToggle $ Net.onlineEvent $ do
+      toggle $ Net.onlineEvent $ do
         time <- getCurrentTime
         setHTML (toJSString $ "Online at: " ++ show time) result
+
+    "h1" </ text "Status Bar"
+    action "Overlay web view" $ Bar.overlaysWebView True
+    action "Don't overlay web view" $ Bar.overlaysWebView False
+    action "Style: default" Bar.styleDefault
+    action "Style: light content" Bar.styleLightContent
+    action "Style: black translucent" Bar.styleBlackTranslucent
+    action "Style: black opaque" Bar.styleBlackOpaque
+    "form" </ do
+      txt <- textBox
+      button $ do
+        text "Set background color by name"
+        onclick $ getValue txt >>= Bar.backgroundColorByName . fromJSString
+    "form" </ do
+      txt <- textBox
+      button $ do
+        text "Set background color by hex string"
+        onclick $ getValue txt >>= Bar.backgroundColorByHexString . fromJSString
+    do
+      result <- "p" <-/ text "Result here"
+      let update = do
+            b <- Bar.isVisible
+            setHTML (toJSString $ "Visible: " ++ show b) result
+      action "Hide" $ Bar.hideBar >> update
+      action "Show" $ Bar.showBar >> update
