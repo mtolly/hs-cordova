73b2c61cfbde1777f568eed982b214f0e293220f
Michael Tolly <miketolly@gmail.com>
2014-12-26 02:26:54 -0600

    dialog tests, and better text boxes

--- hs/src/test/Main.hs
+++ hs/src/test/Main.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE LambdaCase #-}
 module Main (main) where
 
@@ -12,6 +11,7 @@ import qualified System.Cordova.Vibration as Vib
 import qualified System.Cordova.NetworkInformation as Net
 import qualified System.Cordova.BatteryStatus as Bat
 import qualified System.Cordova.Camera as Cam
+import qualified System.Cordova.Dialogs as Dia
 import Data.Default (def)
 import Control.Concurrent.MVar (newMVar, takeMVar, putMVar)
 import Data.Function (on)
@@ -21,7 +21,7 @@ import Data.List (groupBy)
 import Text.Read (readMaybe)
 import Data.Time.Clock (getCurrentTime)
 import Control.Monad.IO.Class (MonadIO, liftIO)
-import Control.Monad (forM_, liftM2)
+import Control.Monad (forM_, liftM2, liftM3, liftM4, join)
 
 import HTMLT
 
@@ -52,8 +52,10 @@ main = do
       button :: (MonadIO m) => HTMLT m a -> HTMLT m a
       button act = "button" </ ("type" $= "button") >> act
 
-      textBox :: (MonadIO m) => HTMLT m Element
-      textBox = "input" <-/ "type" $= "text"
+      textBox :: (MonadIO m) => String -> HTMLT m Element
+      textBox s = "input" <-/ do
+        "type" $= "text"
+        "value" $= s
 
       action :: (MonadIO m) => String -> IO () -> HTMLT m ()
       action s act = button $ text s >> onclick act
@@ -96,7 +98,7 @@ main = do
 
     "h1" </ text "Vibration"
     "form" </ do
-      txt <- textBox
+      txt <- textBox "500 100 200"
       action "Vibrate pattern" $ do
         str <- runHTMLT txt getValue
         let ints = mapMaybe readMaybe $ groupBy ((==) `on` isDigit) str
@@ -129,12 +131,12 @@ main = do
     action "Style: black translucent" Bar.styleBlackTranslucent
     action "Style: black opaque" Bar.styleBlackOpaque
     "form" </ do
-      txt <- textBox
+      txt <- textBox "red"
       button $ do
         text "Set background color by name"
         onclick $ runHTMLT txt getValue >>= Bar.backgroundColorByName
     "form" </ do
-      txt <- textBox
+      txt <- textBox "#ff00ff"
       button $ do
         text "Set background color by hex string"
         onclick $ runHTMLT txt getValue >>= Bar.backgroundColorByHexString
@@ -183,3 +185,31 @@ main = do
         res <- Cam.cleanup
         time <- getCurrentTime
         runHTMLT result $ setHTML $ show res ++ " at " ++ show time
+
+    "h1" </ text "Dialogs"
+    buttonPushed <- "p" <-/ text "Button here"
+    let val :: (Read a) => Element -> IO a
+        val elt = fmap read $ runHTMLT elt getValue
+        push :: (Show a) => a -> IO ()
+        push i = do
+          time <- getCurrentTime
+          runHTMLT buttonPushed $ setHTML $ show i ++ " at " ++ show time
+    "form" </ do
+      t1 <- textBox $ show "The message"
+      t2 <- textBox $ show $ Just "The title"
+      t3 <- textBox $ show $ Just "The button"
+      action "Alert" $ join (liftM3 Dia.alert (val t1) (val t2) (val t3)) >>= push
+    "form" </ do
+      t1 <- textBox $ show "The message"
+      t2 <- textBox $ show $ Just "The title"
+      t3 <- textBox $ show $ Just ["B1", "B2", "B3"]
+      action "Confirm" $ join (liftM3 Dia.confirm (val t1) (val t2) (val t3)) >>= push
+    "form" </ do
+      t1 <- textBox $ show "The message"
+      t2 <- textBox $ show $ Just "The title"
+      t3 <- textBox $ show $ Just ["B1", "B2", "B3"]
+      t4 <- textBox $ show $ Just "Input"
+      action "Prompt" $ join (liftM4 Dia.prompt (val t1) (val t2) (val t3) (val t4)) >>= push
+    "form" </ do
+      t1 <- textBox "2"
+      action "Beep" $ val t1 >>= Dia.beep
